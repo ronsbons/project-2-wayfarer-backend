6070ad132591cff295b4222cf96b1af37f38d8d6
e-morales <e.morales@digitalnest.org>
2019-03-06 15:10:54 -0800

    added app.use cors

--- server.js
+++ server.js
@@ -9,11 +9,7 @@ const db = require("./models");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors())
 
 app.use(express.static("public"));
 
