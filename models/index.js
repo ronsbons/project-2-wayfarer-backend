dc78225f47f0259f857f39eff58eb36542ae8909
lvmor <lvm.lspa@gmail.com>
2019-03-06 07:06:07 -0800

    adds wayfarer database

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/3001", {useNewUrlParser: true});
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wayfarer", {
+  useNewUrlParser: true
+});
 
-module.exports.Users = require('./userModel.js');
-module.exports.Posts = require('./postModel.js');
-module.exports.Cities = require('./cityModel.js');
+module.exports.Users = require("./userModel.js");
+module.exports.Posts = require("./postModel.js");
+module.exports.Cities = require("./cityModel.js");
