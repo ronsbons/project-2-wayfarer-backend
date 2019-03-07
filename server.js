141bbe5a3c2f95492bffca64aa421959bc251a01
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-06 18:00:51 -0800

    Commiting working server.js file

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(cors());
 
 app.use('/user', userRoutes);
 
-app.listen(process.env.PORT || 3001, () =>
-  console.log("Listening on port 3001 :)")
-);
+
 
 app.get("/api", (req, res) => {
   res.json({
@@ -226,6 +224,6 @@ app.delete("/api/cities/:id", (req, res) => {
   });
 });
 
-app.listen(process.env.PORT || 3002, () =>
-  console.log("Listening on port 3002 :)")
+app.listen(process.env.PORT || 3001, () =>
+  console.log("Listening on port 3001 :)")
 );
