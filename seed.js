7a17f428f2477d59bb4e45a776ec3c63ff5a8361
lvmor <lvm.lspa@gmail.com>
2019-03-07 06:12:16 -0800

    matches seed.js information with seed2.js city information

--- seed.js
+++ seed.js
@@ -1,60 +1,24 @@
 const db = require("./models");
 
-<<<<<<< HEAD
-=======
-const myUsers = [
-  {
-    userFullName: "Mary",
-    username: "maryb",
-    userEmail: "dfnsivs@gmail.com",
-    userPassword: "something",
-    userCity: "napa",
-    userPhoto: "pho",
-    userJoinDate: Date()
-  }, //'2019-03-05'
-
-  {
-    userFullName: "John",
-    username: "johncd",
-    userEmail: "johncs@gmail.com",
-    userPassword: "fsfs",
-    userCity: "de",
-    userPhoto: "im",
-    userJoinDate: Date()
-  }
-];
-
-const myCities = [
-  {
-    cityName: "Colorado",
-    cityPhoto: "photo.png"
-  },
-  {
-    cityName: "Wisconsin",
-    cityPhoto: "image.png"
-  }
-];
-
->>>>>>> 6070ad132591cff295b4222cf96b1af37f38d8d6
 const myPosts = [
   {
-    postTitle: "Colorado Trip Post",
+    postTitle: "San Francisco Trip Post",
     postContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
     postDate: "2018-3-12",
     user: "5c804261bec0244d32b761c6",
     city: {
-      cityName: "Colorado",
-      cityPhoto: "colorado.png"
+      cityName: "San Francisco",
+      cityPhoto: "san_francisco.png"
     }
   },
   {
-    postTitle: "Wisconsin Trip Post",
+    postTitle: "London Trip Post",
     postContent: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-    postDate: "2018-3-12",
+    postDate: "2018-3-20",
     user: "5c804261bec0244d32b761c6",
     city: {
-      cityName: "Wisconsin",
-      cityPhoto: "wisconsin.png"
+      cityName: "London",
+      cityPhoto: "london.png"
     }
   }
 ];
